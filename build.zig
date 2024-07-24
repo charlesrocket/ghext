e42b614dc4232a36feab4ca53da3e917e251527f
-k <slowdive@me.com>
2024-07-24 14:11:16 -0700

    build: add `lib`

--- build.zig
+++ build.zig
@@ -1,12 +1,26 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const root_source_file = b.path("src/root.zig");
+
+    _ = b.addModule("Ghext", .{ .root_source_file = root_source_file });
+
+    const lib_step = b.step("lib", "Install library");
+    const lib = b.addStaticLibrary(.{
+        .name = "Ghext",
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = root_source_file,
+    });
+
+    const lib_install = b.addInstallArtifact(lib, .{});
+    lib_step.dependOn(&lib_install.step);
+    b.default_step.dependOn(lib_step);
 
     const unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = root_source_file,
         .target = target,
         .optimize = optimize,
     });
@@ -15,12 +29,15 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 
-    const docs = b.addTest("src/root.zig");
-    docs.setBuildMode(mode);
-    docs.emit_docs = .emit;
-
     const docs_step = b.step("docs", "Generate documentation");
-    docs_step.dependOn(&docs.step);
+    const docs_install = b.addInstallDirectory(.{
+        .install_dir = .prefix,
+        .install_subdir = "doc",
+        .source_dir = lib.getEmittedDocs(),
+    });
+ 
+    doc_step.dependOn(&docs_install.step);
+    b.default_step.dependOn(docs_step);
 
     const kcov = b.addSystemCommand(&.{ "kcov", "kcov-out", "--include-path=src" });
     kcov.addArtifactArg(unit_tests);
