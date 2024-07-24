940dbd610f590ed4e722eed9bde22302c3b4246c
-k <slowdive@me.com>
2024-07-24 11:40:11 -0700

    feat: add `hash_short`

--- src/root.zig
+++ src/root.zig
@@ -3,7 +3,8 @@ const fs = std.fs;
 
 pub const Ghext = struct {
     path: []const u8 = ".git/HEAD",
-    head: []const u8 = "none",
+    hash: []const u8 = "none",
+    hash_short: []const u8 = "none",
     allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator) Ghext {
@@ -11,7 +12,7 @@ pub const Ghext = struct {
     }
 
     pub fn read(self: *Ghext) !void {
-        var buffer: [164]u8 = undefined;
+        var buffer: [1024]u8 = undefined;
         const file = try std.fs.cwd().readFile(self.path, &buffer);
         const size = std.mem.replacementSize(u8, file, "ref: ", ".git/");
         const ref = try self.allocator.alloc(u8, size);
@@ -20,14 +21,15 @@ pub const Ghext = struct {
         _ = std.mem.replace(u8, file, "ref: ", ".git/", ref);
 
         const branch = std.mem.trimRight(u8, ref, "\n");
-        const head_tmp = try std.fs.cwd().readFile(branch, &buffer);
-        const head = std.mem.trimRight(u8, head_tmp, "\n");
+        const hash_tmp = try std.fs.cwd().readFile(branch, &buffer);
+        const hash = std.mem.trimRight(u8, hash_tmp, "\n");
 
-        self.head = try std.mem.Allocator.dupe(self.allocator, u8, head);
+        self.hash = try std.mem.Allocator.dupe(self.allocator, u8, hash);
+        self.hash_short = self.hash[0..7];
     }
 
     pub fn deinit(self: *Ghext) void {
-        self.allocator.free(self.head);
+        self.allocator.free(self.hash);
     }
 };
 
@@ -38,4 +40,7 @@ test "read" {
     defer ghx.deinit();
 
     _ = try ghx.read();
+
+    try testing.expect(ghx.hash_short.len == 7);
+    try testing.expect(ghx.hash.len == 40);
 }
