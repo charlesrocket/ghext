b64228bb257b64ddfb546b57bd39baf1e77c0ff9
-k <slowdive@me.com>
2024-07-24 13:24:27 -0700

    build: generate documentation

--- build.zig
+++ build.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -13,6 +14,13 @@ pub fn build(b: *std.Build) void {
     const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
+
+    const docs = b.addTest("src/root.zig");
+    docs.setBuildMode(mode);
+    docs.emit_docs = .emit;
+
+    const docs_step = b.step("docs", "Generate documentation");
+    docs_step.dependOn(&docs.step);
 
     const kcov = b.addSystemCommand(&.{ "kcov", "kcov-out", "--include-path=src" });
     kcov.addArtifactArg(unit_tests);
