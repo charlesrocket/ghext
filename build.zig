659808dcce3cdfdea9f235238d031cfb54c02be9
-k <slowdive@me.com>
2025-08-09 12:14:59 -0400

    build: use `addLibrary()`

--- build.zig
+++ build.zig
@@ -4,34 +4,35 @@ pub const Ghext = @import("src/ghext.zig");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const root_source_file = b.path("src/ghext.zig");
 
-    _ = b.addModule("ghext", .{ .root_source_file = root_source_file });
+    _ = b.addModule("ghext", .{ .root_source_file = b.path("src/ghext.zig") });
 
-    const lib = b.addStaticLibrary(.{
-        .name = "Ghext",
+    const lib_mod = b.createModule(.{
         .root_source_file = b.path("src/ghext.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    const lib = b.addLibrary(.{
+        .name = "Ghext",
+        .linkage = .static,
+        .root_module = lib_mod,
+    });
+
     b.installArtifact(lib);
 
     const unit_tests = b.addTest(.{
-        .root_source_file = root_source_file,
-        .target = target,
-        .optimize = optimize,
+        .root_module = lib_mod,
     });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 
-    const lib_docs = b.addStaticLibrary(.{
+    const lib_docs = b.addLibrary(.{
         .name = "docs",
-        .root_source_file = root_source_file,
-        .target = target,
-        .optimize = optimize,
+        .linkage = .static,
+        .root_module = lib_mod,
     });
 
     const docs_step = b.step("docs", "Generate documentation");
